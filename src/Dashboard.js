4a4bdb27e6d4bb6dab5fd71c1bab236f74385191
Andrew H L Leung <andrewhlleung@gmail.com>
2022-10-22 19:28:48 +0100

    test mongodb

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,5 @@
 
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { GoogleLogin,GoogleLogout } from 'react-google-login';
 import { LoginContext } from './LoginContext';
 
@@ -9,6 +9,36 @@ export default function Dashboard() {
     //console.log('onLogout:', res);
     setLoginSession({})
   }
+
+
+useEffect(()=>{
+  var myHeaders = new Headers();
+  myHeaders.append("api-key", "zjsocERDuMnQOiL1VcIsE2DdVmf4Ux7HsyKKVFRItyXhfFWRpg4Mv0HPgffC2J9X");
+  myHeaders.append("Content-Type", "application/json");
+  
+  var raw = JSON.stringify({
+    "dataSource": "Cluster0",
+    "database": "demo-react-40m",
+    "collection": "note",
+    "filter": {}
+  });
+  
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+  
+  fetch("https://data.mongodb-api.com/app/data-uddjz/endpoint/data/v1/action/find", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+})
+
+
+
   return (
     <>
     {loginSession.profileObj.email}
