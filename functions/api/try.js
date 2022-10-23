52442552a39cc9307940976107ee622e69646897
Andrew H L Leung <andrewhlleung@gmail.com>
2022-10-23 19:24:57 +0100

    sqlite

--- functions/api/try.js
+++ functions/api/try.js
@@ -1,3 +1,6 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./note.sqlite');
+
 export async function onRequest(context) {  // Contents of context object  const {    request, // same as existing Worker API    env, // same as existing Worker API    params, // if filename includes [id] or [[path]]    waitUntil, // same as ctx.waitUntil in existing Worker API    next, // used for middleware or to fetch assets    data, // arbitrary space for passing data between middlewares  } = context;
   // Contents of context object
   const {
@@ -40,6 +43,15 @@ export async function onRequest(context) {  // Contents of context object  const
       ]
   }
   try{
+    (async () => {
+        const result = await db.query("select * from note", []);
+        returnResponse.db_length=result.rows.length;
+    })();
+  }catch(err){
+    returnResponse.response_err=err
+  }
+  /*
+  try{
     const response = await fetch("https://data.mongodb-api.com/app/data-uddjz/endpoint/data/v1/action/find",requestOptions);
     if( response.ok ){
       returnResponse.response_ok="T"
@@ -52,6 +64,7 @@ export async function onRequest(context) {  // Contents of context object  const
   }catch(err){
     returnResponse.response_err=err
   }
+  */
   /*
   await fetch("https://data.mongodb-api.com/app/data-uddjz/endpoint/data/v1/action/find", requestOptions)
     .then(response => response.text())
