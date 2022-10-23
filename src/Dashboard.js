0e7c36286ba1ae8c14e39f83dba8433c153e380d
Andrew H L Leung <andrewhlleung@gmail.com>
2022-10-23 11:25:14 +0100

    list display

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -10,44 +10,52 @@ export default function Dashboard() {
     setLoginSession({})
   }
 
+  const [productList,setProdustList] = useState([]);
+  useEffect(()=>{
+    var myHeaders = new Headers();
+    //myHeaders.append("api-key", "zjsocERDuMnQOiL1VcIsE2DdVmf4Ux7HsyKKVFRItyXhfFWRpg4Mv0HPgffC2J9X");
+    myHeaders.append("Content-Type", "application/json");
+    
+    var raw = JSON.stringify({
+      // "dataSource": "Cluster0",
+      // "database": "demo-react-40m",
+      // "collection": "note",
+      // "filter": {}
+    });
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+      mode:'no-cors'
+    };
+    
+    fetch("https://demo-react-40m.pages.dev/api/try", requestOptions)
+      .then(response => response.text())
+      .then(result => {
+        console.log(result)
+        setProdustList(JSON.parse(result).documents)
+      })
+      .catch(error => console.log('error', error));
 
-useEffect(()=>{
-  var myHeaders = new Headers();
-  //myHeaders.append("api-key", "zjsocERDuMnQOiL1VcIsE2DdVmf4Ux7HsyKKVFRItyXhfFWRpg4Mv0HPgffC2J9X");
-  myHeaders.append("Content-Type", "application/json");
-  
-  var raw = JSON.stringify({
-    "dataSource": "Cluster0",
-    "database": "demo-react-40m",
-    "collection": "note",
-    "filter": {}
-  });
-  
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow',
-    mode:'no-cors'
-  };
-  
-  fetch("https://demo-react-40m.pages.dev/api/try", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-
-})
-
-
+  })
 
   return (
     <>
-    {loginSession.profileObj.email}
-    <GoogleLogout
-      buttonText="Logout"
-      onLogoutSuccess={onLogout}
-    >
-    </GoogleLogout>
-  </>
+      {loginSession.profileObj.email}
+      <GoogleLogout
+        buttonText="Logout"
+        onLogoutSuccess={onLogout}
+      >
+      </GoogleLogout>
+      {
+        productList.map((product)=>{
+          <div>
+            {product.name}
+          </div>
+        })
+      }
+    </>
   )
 }
