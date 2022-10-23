4ce0cb37292481ae6f497bdde3b7df63b5dd09d5
Andrew H L Leung <andrewhlleung@gmail.com>
2022-10-23 15:30:58 +0100

    .

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -14,7 +14,7 @@ export default function Dashboard() {
   useEffect(()=>{
     
     if( process.env.MODE!="production" ){
-      //setProdustList([{"id":1,"name":"蘋果","price":5,"image":"apple.jpg","description":"新鮮蘋果50克，一日一蘋果，醫生遠離我"},{"id":2,"name":"橙","price":3,"image":"orange.jpg","description":"新鮮橙50克，又甜又好吃"},{"id":3,"name":"芒果","price":4,"image":"mango.jpg","description":"新鮮芒500克，宜做甜品"},{"id":4,"name":"西瓜","price":20,"image":"watermelon.jpg","description":"新鮮西瓜2公斤，夏季必備"},{"id":5,"name":"藍梅","price":10,"image":"blueberry.jpg","description":"新鮮藍梅50克，補眼之寶"},{"id":6,"name":"白蘿蔔","price":5,"image":"white-carrot.jpg","description":"新鮮白蘿蔔1公斤，宜煲湯"}])
+      setProdustList([{"id":1,"name":"蘋果","price":5,"image":"apple.jpg","description":"新鮮蘋果50克，一日一蘋果，醫生遠離我"},{"id":2,"name":"橙","price":3,"image":"orange.jpg","description":"新鮮橙50克，又甜又好吃"},{"id":3,"name":"芒果","price":4,"image":"mango.jpg","description":"新鮮芒500克，宜做甜品"},{"id":4,"name":"西瓜","price":20,"image":"watermelon.jpg","description":"新鮮西瓜2公斤，夏季必備"},{"id":5,"name":"藍梅","price":10,"image":"blueberry.jpg","description":"新鮮藍梅50克，補眼之寶"},{"id":6,"name":"白蘿蔔","price":5,"image":"white-carrot.jpg","description":"新鮮白蘿蔔1公斤，宜煲湯"}])
     }else{
       var myHeaders = new Headers();
       //myHeaders.append("api-key", "zjsocERDuMnQOiL1VcIsE2DdVmf4Ux7HsyKKVFRItyXhfFWRpg4Mv0HPgffC2J9X");
