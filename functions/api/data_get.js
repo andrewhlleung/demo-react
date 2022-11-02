a0b2eadd53cc28acdc9544d4d2ce9772bd9e1965
andrewhlleung <andrewhlleung@gmail.com>
2022-11-02 09:01:58 +0000

    Update data_get.js

--- functions/api/data_get.js
+++ functions/api/data_get.js
@@ -1,16 +1,27 @@
 
 export async function onRequest(context) {
+
+      //get the ENV var for the binding 
+      //note this is the variable name you added in the KV bindings in your Cloudflare pages set up.
+      const KV = context.env.ALLOW_TEST;
+
+      //put a variable in place. 
+      KV.put("foo", "bar",{})
+      
+      //The following are commented out and are here an example on how to use it.
+
+      //get a key
+      //const key = await KV.get("foo")
+      //console.log(key)
+      
+      //delete a key
+      //KV.delete("foo")
+      
+      //list the keys
+      const value = await KV.list()
+      console.log(value)
   
     return new Response(JSON.stringify(context));
-  
-  
-  const value = await NOTE_SPACE.get("product_list");
-  if (value === null) {
-    return new Response("Value not found", { status: 404 });
-  }
-
-  return new Response(value);
-  
   }
   
 
